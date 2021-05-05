55e0b75aba9cca4062a7032927d9b5a4fb1ebde5
David de los Santos Boix <ddlsb@hotmail.com>
2021-05-05 23:27:45 +0200

    Revert replaceAll for Firefox

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str, newStr) {
+
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr);
+    }
+
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+
+  };
+}
+
 createApp(App).use(router).mount('#app')
